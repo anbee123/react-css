335a956e79e6ae21f5fa306d7f0b4d7c11491ed6
An Bui <anhipchipchip@gmail.com>
2022-12-08 00:43:21 -0500

    style with sass

--- react-css/src/App.js
+++ react-css/src/App.js
@@ -5,6 +5,7 @@ import Button2 from './components/example-2-inline-css/Button'
 import Button3 from './components/example-3-css-in-js/Button'
 import Button4 from './components/example-4-styled-components/Button'
 import Button5 from './components/example-5-css-modules/Button'
+import Button6 from './components/example-6-sass/Button'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
      {/* example 4 styled components */}
      <Button4 buttonText="Example 4" bgcolor="#86ba8a" />
      < Button5 buttonText="Example 5" />
+     <Button6 buttonText="Example 6" />
     </div>
   );
 }
---
+++ react-css/src/components/example-6-sass/Button.jsx
@@ -0,0 +1,7 @@
+import "./button.sass"
+
+export default function Button({ buttonText }) {
+    return (
+      <button className="button">{ buttonText }</button>
+    );
+  }
